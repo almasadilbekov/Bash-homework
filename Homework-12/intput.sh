e6b0815431bffe992bcd7831131801543b5d9bda
almasadilbekov <almas.adilbekov555@gmail.com>
2025-06-03 19:34:21 +0000

    bash2 files

---
+++ Homework-12/intput.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+read -p "enter your name " name
+read -p "enter your age " age
+read -p "enter your hobby " hobby
+read -p "enter your language " language
+read -p "enter your dream " dream
+
+echo "hello my name is $name, I am $age years old, my hobby is $hobby, I speak $language, also my dream is $dream"
---
+++ Homework-12/introduce.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+my_name=Almas
+my_age=29
+my_lang=eng
+my_hobby=chess
+my_dream=wealth
+
+echo "my name is $my_name, I am $my_age, my language is $my_lang, my hobby is $my_hobby, also my dream is $my_dream"
